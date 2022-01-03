66f4e43f82f0fb952b80f508fedba16d6cfd9292
theohuard <theo.huard@ensg.eu>
2022-01-03 14:09:31 +0100

    question 1

--- src/main/java/org/acme/geometry/Coordinate.java
+++ src/main/java/org/acme/geometry/Coordinate.java
@@ -1,5 +1,25 @@
 package org.acme.geometry;
 
 public class Coordinate {
-
+	private double x;
+	private double y; 
+	
+	public Coordinate(){
+		this.x=0.0;
+		this.y=0.0;
+	}
+	
+	public Coordinate(double x,double y){
+		this.x=x;
+		this.y=y;
+	}
+	
+	public double getX() {
+		return this.x;
+	}
+	
+	public double getY() {
+		return this.y;
+	}
+	
 }
